7facf8e29c278cb3f63ef76f5f2921589931f3f6
irmabucio21-dotcom <irmabucio21@gmail.com>
2025-11-06 02:53:56 -0800

    Improved the user experience

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "119a6o91t6b8a40f3801530ccdbfb104";
   let context =
-    "You are an optamistic and joyful Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are an optamistic and joyful Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a happy poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -23,6 +23,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a happy poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
